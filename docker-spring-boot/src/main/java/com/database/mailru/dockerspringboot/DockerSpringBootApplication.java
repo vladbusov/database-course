af76cd2db9e45b47dbda8972004200bb5f71414a
vladbusov <to2002to2002@gmail.com>
2018-04-10 17:07:12 +0300

    pervonah

---
+++ docker-spring-boot/src/main/java/com/database/mailru/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.database.mailru.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApplication.class, args);
+	}
+}
