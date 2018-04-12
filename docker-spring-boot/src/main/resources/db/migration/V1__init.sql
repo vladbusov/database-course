01dd3bf9ce72150772fcb1a464d9d860f2732460
VladBusov <to2002to2002@gmail.com>
2018-04-12 03:24:55 +0300

    добавил разделы

--- docker-spring-boot/src/main/resources/db/migration/V1__init.sql
+++ docker-spring-boot/src/main/resources/db/migration/V1__init.sql
@@ -1,30 +1,28 @@
 CREATE TABLE users (
-  id SERIAL primary key,
-  nickname VARCHAR(255),
+  nickname VARCHAR(20) primary key,
   email    VARCHAR(255),
   fullname VARCHAR(255),
   about TEXT,
-  UNIQUE (nickname, email)
-);
-
-CREATE TABLE thread (
-  id SERIAL PRIMARY KEY ,
-  slug VARCHAR(255),
-  author VARCHAR(255) REFERENCES users(nickname),
-  forum VARCHAR(255) REFERENCES forum(slug),
-  created TIMESTAMP,
-  message TEXT,
-  title VARCHAR(255),
-  votes INTEGER,
-  UNIQUE (slug)
+  UNIQUE (email)
 );
 
 CREATE TABLE forum (
-  id SERIAL PRIMARY KEY,
-  slug VARCHAR(255) REFERENCES threadModel(slug),
+  slug VARCHAR(255) primary key,
   title VARCHAR(255),
-  user VARCHAR(255) REFERENCES users(nickname),
+  userRef VARCHAR(255) references users (nickname),
   posts INTEGER,
-  threads INTEGER,
-  UNIQUE (title)
+  threads INTEGER
 );
+
+CREATE TABLE thread (
+  id SERIAL PRIMARY KEY,
+  slug VARCHAR(255)  references forum (slug),
+  author VARCHAR(255) references (nickname),
+  forum VARCHAR(255),
+  created TIMESTAMP,
+  message TEXT,
+  title VARCHAR(255),
+  votes INTEGER
+);
+
+
